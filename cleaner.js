25defa485a47128ca23e0b5c8a273e07c74c9ff6
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-08-23 00:48:54 +0200

    Improved simplified implementation

--- cleaner.js
+++ cleaner.js
@@ -1,47 +1,22 @@
-var Future = Npm.require('fibers/future');
-
 resetDatabase = function () {
-
-  if (process.env.NODE_ENV !== 'development') {
-    console.error('[xolvio:cleaner] Detected non-development environment. Aborting database reset');
+  if (!process.env.IS_MIRROR && !process.env.VELOCITY_TEST_PACKAGES) {
+    console.error(
+      'resetDatabase is not allowed outside of a mirror. ' +
+      'Something has gone wrong.'
+    );
     return;
   }
 
-  console.log('Removing collections');
-  var fut = new Future();
-
-  var collectionsRemoved = 0;
   var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
-
-  db.collections(function (err, collections) {
-
-    var appCollections = _.reject(collections, function (col) {
-      return col.collectionName.indexOf('velocity') === 0 ||
-        col.collectionName === 'system.indexes';
-    });
-
-    if (appCollections.length > 0) {
-      _.each(appCollections, function (appCollection) {
-        var collectionCanonicalName = db.databaseName + '.' + appCollection.collectionName;
-        appCollection.remove(function (e) {
-          if (e) {
-            console.error('Failed removing collection', collectionCanonicalName, e);
-            fut.return('fail: ' + e);
-          }
-          collectionsRemoved++;
-          console.log('Removed', collectionCanonicalName);
-          if (appCollections.length === collectionsRemoved) {
-            console.log('Successfully removed collections');
-            fut['return']('success');
-          }
-        });
-      });
-    } else {
-      console.log('No collections found to remove');
-      fut['return']('success');
-    }
-
+  var getCollections = Meteor.wrapAsync(db.collections, db);
+  var collections = getCollections();
+  var appCollections = _.reject(collections, function (col) {
+    return col.collectionName.indexOf('velocity') === 0 ||
+      ['system.indexes'].indexOf(col.collectionName) !== -1;
   });
 
-  return fut.wait();
+  _.each(appCollections, function (appCollection) {
+    var remove = Meteor.wrapAsync(appCollection.remove, appCollection);
+    remove({});
+  });
 };
