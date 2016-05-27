5367225ad5eb0d89932826b2c7145455a191fef2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-05-27 18:41:36 +0200

    Release of 0.3.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'xolvio:cleaner',
   summary: 'Gives you methods to clear your Mongo database and collections for testing purposes',
-  version: '0.3.0',
+  version: '0.3.1',
   git: 'https://github.com/xolvio/cleaner.git',
   documentation: 'README.md',
   debugOnly: true,
